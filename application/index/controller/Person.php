bd5bd1620b8073057ac7b2a04b1cd8265479efe7
forever-star <17710388893@163.com>
2017-06-14 23:53:09 +0800

    创建个人中心界面
    添加person控制器

---
+++ application/index/controller/Person.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\index\controller;
+
+class Person extends \think\Controller
+{
+
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+
+    public function creatline()
+    {
+        return $this->fetch();
+    }
+
+}
