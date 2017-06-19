37ea497def208de08f7d714931e09e01d44ea2ce
zhangyang <zhangyang59@163.com>
2017-06-19 13:30:58 +0800

    短信发送示例 index

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,6 +7,7 @@ class Index extends \think\Controller
 		public function index()
 		    {
 
+//          短信发送示例
 //		        $datas=["45611"];
 //
 //                sendTemplateSMS("13393758290",$datas,"29760");
