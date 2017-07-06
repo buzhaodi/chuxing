9f5c835e8bebcba825b866fd1c6a6ecd07f42895
zhangyang <zhangyang59@163.com>
2017-07-06 12:00:29 +0800

    增加修改个人信息页面

--- application/index/controller/Seting.php
+++ application/index/controller/Seting.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\index\controller;
 
+use think\Session;
+
 class Seting extends Publiccon
 {
 
@@ -26,6 +28,43 @@ class Seting extends Publiccon
 		        $this->assign("imchezhus",$imchezhu);
 		    	return $this->fetch();
 		    }
+
+		    public function changeuser(){
+		    $data=input();
+		    $data=array_filter($data);
+		    $res=db("user")->update($data);
+		    if($res){
+		        Session::clear();
+                return $this->success("修改成功");
+            }
+            else{
+                return $this->success("修改失败");
+            }
+
+            }
+
+            public function getheadimg(){
+                $file = request()->file('headimg');
+                // 移动到框架应用根目录/public/uploads/ 目录下
+                $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+                if($info){
+                    // 成功上传后 获取上传信息
+                    // 输出 jpg
+
+                  $src="/public/uploads/".$info->getSaveName();
+                    $src=str_replace("\\","/",$src);
+                  return json(array("status"=>'success','url'=>$src));
+
+
+
+                }else{
+                    // 上传失败获取错误信息
+
+                    return json(array("status"=>'error','msg'=>$file->getError()));
+                }
+            }
+
+
 
         public function auth(){
             return $this->fetch();
